b8f46819a2a5318926dd607a9f47c7a0aae0f0a6
ttblanchard <55503092+ttblanchard@users.noreply.github.com>
2023-09-01 11:26:56 -0500

    fix(storageprofile): Fix error when loading storage profiles with NSCC Profile (#18)
    * fix(storageprofile): Fix error when loading storage profiles with NSCC profile
    
    * fix test

--- test/unit/deadline_client/api/test_api_storage_profile.py
+++ test/unit/deadline_client/api/test_api_storage_profile.py
@@ -54,11 +54,8 @@ def test_list_storage_profiles_for_queue_paginated(fresh_deadline_config):
         assert storage_profiles["storageProfiles"] == STORAGE_PROFILES_LIST
 
 
-@pytest.mark.parametrize("pass_principal_id_filter", [True, False])
 @pytest.mark.parametrize("user_identities", [True, False])
-def test_list_storage_profiles_for_queue_principal_id(
-    fresh_deadline_config, pass_principal_id_filter, user_identities
-):
+def test_list_storage_profiles_for_queue(fresh_deadline_config, user_identities):
     """Confirm api.list_storage_profiles_for_queue sets the principalId parameter appropriately"""
 
     with patch.object(api._session, "get_boto3_session") as session_mock:
@@ -72,23 +69,8 @@ def test_list_storage_profiles_for_queue_principal_id(
                 "identity_store_id": "idstoreid",
             }
 
-        # Call the API
-        if pass_principal_id_filter:
-            storage_profiles = api.list_storage_profiles_for_queue(principalId="otheruserid")
-        else:
-            storage_profiles = api.list_storage_profiles_for_queue()
+        storage_profiles = api.list_storage_profiles_for_queue()
 
         assert storage_profiles["storageProfiles"] == STORAGE_PROFILES_LIST
 
-        if pass_principal_id_filter:
-            session_mock().client(
-                "deadline"
-            ).list_storage_profiles_for_queue.assert_called_once_with(principalId="otheruserid")
-        elif user_identities:
-            session_mock().client(
-                "deadline"
-            ).list_storage_profiles_for_queue.assert_called_once_with(principalId="userid")
-        else:
-            session_mock().client(
-                "deadline"
-            ).list_storage_profiles_for_queue.assert_called_once_with()
+        session_mock().client("deadline").list_storage_profiles_for_queue.assert_called_once_with()
