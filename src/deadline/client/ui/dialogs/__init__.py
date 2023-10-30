9d1a9252d9f8de53c855a65a23166fd2524c0b8c
Ramon Montoya Vozmediano <1171039+rmv@users.noreply.github.com>
2023-10-30 14:22:42 -0700

    feat: Add a 'JobBundlePurpose' type. (#94)
    Signed-off-by: Ramon Montoya Vozmediano <1171039+rmv@users.noreply.github.com>

--- src/deadline/client/ui/dialogs/__init__.py
+++ src/deadline/client/ui/dialogs/__init__.py
@@ -1,5 +1,9 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
+from ._types import (
+    JobBundlePurpose,  # noqa: F401
+)
+
 __all__ = ["DeadlineConfigDialog", "DeadlineLoginDialog"]
 
 from .deadline_config_dialog import DeadlineConfigDialog
