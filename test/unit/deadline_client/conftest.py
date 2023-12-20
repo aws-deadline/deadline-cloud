50ddb985c812f4507e54a28ab9be1e6fb545ed64
Gahyun Suh <132245153+gahyusuh@users.noreply.github.com>
2023-12-20 14:52:12 -0600

    feat(job_attachments): make hard-coded parameters configurable (#125)
    Signed-off-by: Gahyun Suh <gahyusuh@dev-dsk-gahyusuh-2a-387e0b51.us-west-2.amazon.com>

--- test/unit/deadline_client/conftest.py
+++ test/unit/deadline_client/conftest.py
@@ -5,34 +5,7 @@ Common fixtures for Deadline Client Library tests.
 """
 
 import tempfile
-from unittest.mock import patch
-from pathlib import Path
-
 import pytest
-
-from deadline.client.config import config_file
-
-
-@pytest.fixture(scope="function")
-def fresh_deadline_config():
-    """
-    Fixture to start with a blank Amazon Deadline Cloud config file.
-    """
-
-    try:
-        # Create an empty temp file to set as the Amazon Deadline Cloud config
-        temp_dir = tempfile.TemporaryDirectory()
-        temp_dir_path = Path(temp_dir.name)
-        temp_file_path = temp_dir_path / "config"
-        with open(temp_file_path, "w+t", encoding="utf8") as temp_file:
-            temp_file.write("")
-
-        # Yield the temp file name with it patched in as the
-        # Amazon Deadline Cloud config file
-        with patch.object(config_file, "CONFIG_FILE_PATH", str(temp_file_path)):
-            yield str(temp_file_path)
-    finally:
-        temp_dir.cleanup()
 
 
 @pytest.fixture(scope="function")
