152f662f8aa5fd37cbebf114ab3bb2d9800b017f
Nathan MacLeod <142927985+npmacl@users.noreply.github.com>
2024-03-13 19:12:12 +0000

    feat: enable cache for VFS
    Signed-off-by: Nathan MacLeod <142927985+npmacl@users.noreply.github.com>

--- src/deadline/job_attachments/asset_manifests/decode.py
+++ src/deadline/job_attachments/asset_manifests/decode.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 import json
+import re
 from pathlib import Path
 from typing import Any, Optional, Tuple
 
@@ -31,6 +32,7 @@ def validate_manifest(
     """
     try:
         jsonschema.validate(manifest, _get_schema(version))
+
     except (jsonschema.ValidationError, jsonschema.SchemaError) as e:
         return False, str(e)
 
@@ -67,5 +69,13 @@ def decode_manifest(manifest: str) -> BaseAssetManifest:
         raise ManifestDecodeValidationError(error_string)
 
     manifest_model = ManifestModelRegistry.get_manifest_model(version=version)
+    decoded_manifest = manifest_model.AssetManifest.decode(manifest_data=document)
 
-    return manifest_model.AssetManifest.decode(manifest_data=document)
+    # Validate hashes are alphanumeric
+    for path in decoded_manifest.paths:
+        if re.fullmatch("[a-zA-Z0-9]+", path.hash) is None:
+            raise ManifestDecodeValidationError(
+                f"The hash {path.hash} for path {path.path} is not alphanumeric"
+            )
+
+    return decoded_manifest
--- test/unit/deadline_job_attachments/asset_manifests/v2023_03_03/test_asset_manifest.py
+++ test/unit/deadline_job_attachments/asset_manifests/v2023_03_03/test_asset_manifest.py
@@ -21,16 +21,16 @@ def test_encode():
             ManifestPath(path="test_file", hash="a", size=1, mtime=167907934333848),
             ManifestPath(path="test_dir/test_file", hash="b", size=1, mtime=1479079344833848),
             ManifestPath(path="another_test_file", hash="c", size=1, mtime=1675079344833848),
-            ManifestPath(path="€", hash="Euro Sign", size=1, mtime=1679079344836848),
-            ManifestPath(path="\r", hash="Carriage Return", size=1, mtime=1679079744833848),
+            ManifestPath(path="€", hash="EuroSign", size=1, mtime=1679079344836848),
+            ManifestPath(path="\r", hash="CarriageReturn", size=1, mtime=1679079744833848),
             ManifestPath(
-                path="דּ", hash="Hebrew Letter Dalet With Dagesh", size=1, mtime=1679039344833848
+                path="דּ", hash="HebrewLetterDaletWithDagesh", size=1, mtime=1679039344833848
             ),
             ManifestPath(path="1", hash="One", size=1, mtime=1679079344833868),
-            ManifestPath(path="😀", hash="Emoji: Grinning Face", size=1, mtime=1679579344833848),
+            ManifestPath(path="😀", hash="EmojiGrinningFace", size=1, mtime=1679579344833848),
             ManifestPath(path="\u0080", hash="Control", size=1, mtime=1679079344833348),
             ManifestPath(
-                path="ö", hash="Latin Small Letter O With Diaeresis", size=1, mtime=1679079344833848
+                path="ö", hash="LatinSmallLetterOWithDiaeresis", size=1, mtime=1679079344833848
             ),
         ],
     )
@@ -40,16 +40,16 @@ def test_encode():
         '"hashAlg":"xxh128",'
         '"manifestVersion":"2023-03-03",'
         '"paths":['
-        r'{"hash":"Carriage Return","mtime":1679079744833848,"path":"\r","size":1},'
+        r'{"hash":"CarriageReturn","mtime":1679079744833848,"path":"\r","size":1},'
         '{"hash":"One","mtime":1679079344833868,"path":"1","size":1},'
         '{"hash":"c","mtime":1675079344833848,"path":"another_test_file","size":1},'
         '{"hash":"b","mtime":1479079344833848,"path":"test_dir/test_file","size":1},'
         '{"hash":"a","mtime":167907934333848,"path":"test_file","size":1},'
         r'{"hash":"Control","mtime":1679079344833348,"path":"\u0080","size":1},'
-        r'{"hash":"Latin Small Letter O With Diaeresis","mtime":1679079344833848,"path":"\u00f6","size":1},'
-        r'{"hash":"Euro Sign","mtime":1679079344836848,"path":"\u20ac","size":1},'
-        r'{"hash":"Emoji: Grinning Face","mtime":1679579344833848,"path":"\ud83d\ude00","size":1},'
-        r'{"hash":"Hebrew Letter Dalet With Dagesh","mtime":1679039344833848,"path":"\ufb33","size":1}'
+        r'{"hash":"LatinSmallLetterOWithDiaeresis","mtime":1679079344833848,"path":"\u00f6","size":1},'
+        r'{"hash":"EuroSign","mtime":1679079344836848,"path":"\u20ac","size":1},'
+        r'{"hash":"EmojiGrinningFace","mtime":1679579344833848,"path":"\ud83d\ude00","size":1},'
+        r'{"hash":"HebrewLetterDaletWithDagesh","mtime":1679039344833848,"path":"\ufb33","size":1}'
         "],"
         '"totalSize":10'
         "}"
@@ -67,19 +67,19 @@ def test_decode(default_manifest_str_v2023_03_03: str):
         hash_alg=HashAlgorithm("xxh128"),
         total_size=10,
         paths=[
-            ManifestPath(path="\r", hash="Carriage Return", size=1, mtime=1679079744833848),
+            ManifestPath(path="\r", hash="CarriageReturn", size=1, mtime=1679079744833848),
             ManifestPath(path="1", hash="One", size=1, mtime=1679079344833868),
             ManifestPath(path="another_test_file", hash="c", size=1, mtime=1675079344833848),
             ManifestPath(path="test_dir/test_file", hash="b", size=1, mtime=1479079344833848),
             ManifestPath(path="test_file", hash="a", size=1, mtime=167907934333848),
             ManifestPath(path="\u0080", hash="Control", size=1, mtime=1679079344833348),
             ManifestPath(
-                path="ö", hash="Latin Small Letter O With Diaeresis", size=1, mtime=1679079344833848
+                path="ö", hash="LatinSmallLetterOWithDiaeresis", size=1, mtime=1679079344833848
             ),
-            ManifestPath(path="€", hash="Euro Sign", size=1, mtime=1679079344836848),
-            ManifestPath(path="😀", hash="Emoji: Grinning Face", size=1, mtime=1679579344833848),
+            ManifestPath(path="€", hash="EuroSign", size=1, mtime=1679079344836848),
+            ManifestPath(path="😀", hash="EmojiGrinningFace", size=1, mtime=1679579344833848),
             ManifestPath(
-                path="דּ", hash="Hebrew Letter Dalet With Dagesh", size=1, mtime=1679039344833848
+                path="דּ", hash="HebrewLetterDaletWithDagesh", size=1, mtime=1679039344833848
             ),
         ],
     )
