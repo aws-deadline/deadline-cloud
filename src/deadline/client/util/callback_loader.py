31144db23b70d80c54da8c1dd426be34d386d334
Alex Hughes <ahughesalex@gmail.com>
2024-04-18 16:12:28 -0700

    Optional extension of the callback loader to validate function params
    This implements `validate_function_signature`  which takes a loaded function and compares it's signature to an OrderedDict of parameters. By default this will compare it to the signature of `_reference_callback_signature`.
    We've specified the `settings` parameter as an object type instead of any specific settings type and then we compare the bases of the passed arguments and determine if it is contained in the class hierarchy.
    
    Signed-off-by: Alex Hughes <ahughesalex@gmail.com>

--- src/deadline/client/util/callback_loader.py
+++ src/deadline/client/util/callback_loader.py
@@ -1,5 +1,30 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 import importlib.util
+import inspect
 import sys
+from typing import Any, Optional, get_type_hints
+
+from deadline.client.ui.dialogs.submit_job_to_deadline_dialog import (  # type: ignore
+    SubmitJobToDeadlineDialog,
+    JobBundlePurpose,
+)
+from deadline.client.job_bundle.submission import AssetReferences
+
+
+def _reference_callback_signature(
+    widget: SubmitJobToDeadlineDialog,
+    job_bundle_dir: str,
+    settings: object,
+    queue_parameters: list[dict[str, Any]],
+    asset_references: AssetReferences,
+    host_requirements: Optional[dict[str, Any]] = None,
+    purpose: JobBundlePurpose = JobBundlePurpose.SUBMISSION,
+):
+    pass
+
+
+CALLBACK_REFERENCE_SIGNATURE = inspect.signature(_reference_callback_signature).parameters
+CALLBACK_REFERENCE_HINTS = get_type_hints(_reference_callback_signature)
 
 
 def import_module_function(module_path, module_name, function_name):
@@ -8,3 +33,29 @@ def import_module_function(module_path, module_name, function_name):
     sys.modules[module_name] = mod
     spec.loader.exec_module(mod)
     return getattr(mod, function_name)
+
+
+def _validate_parameter_type(parameter_type, other_type):
+    if parameter_type == other_type:
+        return True
+
+    if parameter_type in other_type.__bases__:
+        return True
+    return False
+
+
+def validate_function_signature(function, reference_signature=CALLBACK_REFERENCE_SIGNATURE):
+    parameters = inspect.signature(function).parameters
+    if parameters == reference_signature:
+        return True
+
+    function_hints = get_type_hints(function)
+
+    for param_name in CALLBACK_REFERENCE_HINTS:
+        if not function_hints.get(param_name):
+            return False
+        if not _validate_parameter_type(
+            CALLBACK_REFERENCE_HINTS[param_name], function_hints[param_name]
+        ):
+            return False
+    return True
