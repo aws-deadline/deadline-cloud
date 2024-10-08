50e9280689030d06e4465a8a438d8241d2e4d99f
David Leong <116610336+leongdl@users.noreply.github.com>
2024-10-08 16:24:48 -0700

    feat(JA): Implementation for Job Attachments diff command
    Signed-off-by: David Leong <116610336+leongdl@users.noreply.github.com>

--- src/deadline/job_attachments/api/manifest.py
+++ src/deadline/job_attachments/api/manifest.py
@@ -2,6 +2,7 @@
 
 import datetime
 import os
+from pathlib import Path
 from typing import List, Optional, Tuple
 
 from deadline.client.cli._groups.click_logger import ClickLogger
@@ -11,22 +12,58 @@ from deadline.job_attachments.asset_manifests._create_manifest import (
     _create_manifest_for_single_root,
 )
 from deadline.job_attachments.asset_manifests.base_manifest import (
+    BaseAssetManifest,
     BaseManifestPath,
 )
+from deadline.client.config import config_file
 from deadline.job_attachments.asset_manifests.decode import decode_manifest
 from deadline.job_attachments.asset_manifests.hash_algorithms import hash_data
+from deadline.job_attachments.caches.hash_cache import HashCache
 from deadline.job_attachments.models import (
     FileStatus,
     GlobConfig,
+    ManifestDiff,
     ManifestSnapshot,
     default_glob_all,
 )
+from deadline.job_attachments.upload import S3AssetManager
 
 """
 APIs here should be business logic only. It should perform one thing, and one thing well. 
 It should use basic primitives like S3 upload, download, boto3 APIs.
 These APIs should be boto3 session agnostic and a specific Boto3 Credential to use.
 """
+
+
+def _glob_files(
+    root: str,
+    include: Optional[List[str]] = None,
+    exclude: Optional[List[str]] = None,
+    include_exclude_config: Optional[str] = None,
+) -> List[str]:
+    """
+    :param include: Include glob to look for files to add to the manifest.
+    :param exclude: Exclude glob to exclude files from the manifest.
+    :param include_exclude_config: Config JSON or file containeing input and exclude config.
+    :returns: All files matching the include and exclude expressions.
+    """
+
+    # Get all files in the root.
+    glob_config: GlobConfig
+    if include or exclude:
+        include = include if include is not None else default_glob_all()
+        exclude = exclude if exclude is not None else []
+        glob_config = GlobConfig(include_glob=include, exclude_glob=exclude)
+    elif include_exclude_config:
+        glob_config = _process_glob_inputs(include_exclude_config)
+    else:
+        # Default, include all.
+        glob_config = GlobConfig()
+
+    input_files = _glob_paths(
+        root, include=glob_config.include_glob, exclude=glob_config.exclude_glob
+    )
+    return input_files
 
 
 def _manifest_snapshot(
@@ -77,9 +114,17 @@ def _manifest_snapshot(
 
         # Fast comparison using time stamps and sizes.
         if not force_rehash:
-            changed_paths = _fast_file_list_to_manifest_diff(
-                root, current_files, source_manifest, logger
+            diff_list: List[Tuple[str, FileStatus]] = _fast_file_list_to_manifest_diff(
+                root=root,
+                current_files=current_files,
+                diff_manifest=source_manifest,
+                logger=logger,
+                return_root_relative_path=False,
             )
+            for diff_file in diff_list:
+                # Add all new and modified
+                if diff_file[1] != FileStatus.DELETED:
+                    changed_paths.append(diff_file[0])
         else:
             # In "slow / thorough" mode, we check by hash, which is definitive.
             output_manifest = _create_manifest_for_single_root(
@@ -128,3 +173,78 @@ def _manifest_snapshot(
         # No manifest generated.
         logger.echo("No manifest generated")
         return None
+
+
+def _manifest_diff(
+    manifest: str,
+    root: str,
+    include: Optional[List[str]] = None,
+    exclude: Optional[List[str]] = None,
+    include_exclude_config: Optional[str] = None,
+    force_rehash=False,
+    logger: ClickLogger = ClickLogger(False),
+) -> ManifestDiff:
+    """
+    BETA API - This API is still evolving but will be made public in the near future.
+    API to diff a manifest root with a previously snapshotted manifest.
+    :param manifest: Manifest file path to compare against.
+    :param root: Root directory to generate the manifest fileset.
+    :param include: Include glob to look for files to add to the manifest.
+    :param exclude: Exclude glob to exclude files from the manifest.
+    :param include_exclude_config: Config JSON or file containeing input and exclude config.
+    :param logger: Click Logger instance to print to CLI as test or JSON.
+    :returns: ManifestDiff object containing all new changed, deleted files.
+    """
+
+    # Find all files matching our regex
+    input_files = _glob_files(
+        root=root, include=include, exclude=exclude, include_exclude_config=include_exclude_config
+    )
+    input_paths = [Path(p) for p in input_files]
+
+    # Placeholder Asset Manager
+    asset_manager = S3AssetManager()
+
+    # parse the given manifest to compare against.
+    local_manifest_object: BaseAssetManifest
+    with open(manifest) as input_file:
+        manifest_data_str = input_file.read()
+        local_manifest_object = decode_manifest(manifest_data_str)
+
+    output: ManifestDiff = ManifestDiff()
+
+    if force_rehash:
+        # hash and create manifest of local directory
+        cache_config = config_file.get_cache_directory()
+        with HashCache(cache_config) as hash_cache:
+            directory_manifest_object = asset_manager._create_manifest_file(
+                input_paths=input_paths, root_path=root, hash_cache=hash_cache
+            )
+
+        # Hash based compare manifests.
+        differences: List[Tuple[FileStatus, BaseManifestPath]] = compare_manifest(
+            reference_manifest=local_manifest_object, compare_manifest=directory_manifest_object
+        )
+        # Map to output datastructure.
+        for item in differences:
+            if item[0] == FileStatus.MODIFIED:
+                output.modified.append(item[1].path)
+            elif item[0] == FileStatus.NEW:
+                output.new.append(item[1].path)
+            elif item[0] == FileStatus.DELETED:
+                output.deleted.append(item[1].path)
+
+    else:
+        # File based comparisons.
+        fast_diff: List[Tuple[str, FileStatus]] = _fast_file_list_to_manifest_diff(
+            root=root, current_files=input_files, diff_manifest=local_manifest_object, logger=logger
+        )
+        for fast_diff_item in fast_diff:
+            if fast_diff_item[1] == FileStatus.MODIFIED:
+                output.modified.append(fast_diff_item[0])
+            elif fast_diff_item[1] == FileStatus.NEW:
+                output.new.append(fast_diff_item[0])
+            elif fast_diff_item[1] == FileStatus.DELETED:
+                output.deleted.append(fast_diff_item[0])
+
+    return output
