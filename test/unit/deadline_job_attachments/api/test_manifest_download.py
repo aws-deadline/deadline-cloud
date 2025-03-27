5ecd7ec9ac7c1101cd45587ef35c9853d5e061b9
Stephen Crowe <6042774+crowecawcaw@users.noreply.github.com>
2025-03-27 11:07:36 -0700

    fix: allow pagination over step dependencies in manifest download cli (#638)
    Signed-off-by: Yutong Li <52769999+YutongLi291@users.noreply.github.com>

--- test/unit/deadline_job_attachments/api/test_manifest_download.py
+++ test/unit/deadline_job_attachments/api/test_manifest_download.py
@@ -73,6 +73,65 @@ class TestManifestDownload:
             farm_id="farm-12345",
             queue_id="queue-12345",
             job_id="job-12345",
+            step_id="step-12345",
             boto3_session=mock_boto_session,
         )
         assert output is not None
+
+        # list_step_dependencies should have been called once as there is no pagination
+        assert mock_deadline_client.list_step_dependencies.call_count == 1
+
+    @patch("deadline.job_attachments.api.manifest._get_queue_user_boto3_session")
+    @patch("deadline.job_attachments.api.manifest.get_manifest_from_s3")
+    @patch("deadline.job_attachments.api.manifest.get_output_manifests_by_asset_root")
+    def test_download_job_paginate_through_step_dependencies(
+        self,
+        mock_get_output_manifest: MagicMock,
+        mock_get_manifest_from_s3: MagicMock,
+        mock_queue_session: MagicMock,
+        temp_dir: str,
+    ):
+        # This is heavily mocked, so return nothing. Integration tests tests full manifest merging.
+        mock_get_manifest_from_s3.return_value = None
+        mock_get_output_manifest.return_value = {}
+
+        # Mock Boto
+        mock_boto_session = MagicMock()
+
+        # Mock Get Queue Credentials
+        mock_queue_session.return_value = MagicMock()
+
+        # Mock up Deadline.
+        mock_deadline_client = MagicMock()
+        mock_boto_session.client.return_value = mock_deadline_client
+
+        # Mock the result of get_queue
+        mock_deadline_client.get_queue.return_value = {
+            "displayName": "queue",
+            "jobAttachmentSettings": {"s3BucketName": "bucket", "rootPrefix": "root_prefix"},
+        }
+        # Mock the result of get_job
+        mock_deadline_client.get_job.return_value = {
+            "name": "Mock Job",
+            "attachments": {
+                "manifests": [{"inputManifestPath": "s3://hello/world", "rootPath": "/some/root"}],
+            },
+        }
+        # Mock the result of list_step_dependencies, have a nextToken to make sure that our code paginates
+        mock_deadline_client.list_step_dependencies.side_effect = [
+            {"dependencies": [{"stepId": f"step-{i}"} for i in range(100)], "nextToken": "abcasd"},
+            {"dependencies": [{"stepId": f"step-{i}"} for i in range(100, 150)]},
+        ]
+
+        output: ManifestDownloadResponse = _manifest_download(
+            download_dir=temp_dir,
+            farm_id="farm-12345",
+            queue_id="queue-12345",
+            job_id="job-12345",
+            step_id="step-12345",
+            boto3_session=mock_boto_session,
+        )
+        assert output is not None
+
+        # list_step_dependencies should have been called twice to paginate
+        assert mock_deadline_client.list_step_dependencies.call_count == 2
