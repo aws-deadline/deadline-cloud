6aa7f4cd0d1817df6971f075faaa0d5fa6dbed60
Stephen Crowe <6042774+crowecawcaw@users.noreply.github.com>
2025-05-15 11:01:22 -0500

    fix: Guarantee string input to line edit GUI fields (#678)
    fix: Guarantee string input to line edit GUI fields
    
    Signed-off-by: Mia Battad <133708990+miabatta@users.noreply.github.com>

--- test/unit/deadline_client/ui/widgets/test_openjd_parameters_widget.py
+++ test/unit/deadline_client/ui/widgets/test_openjd_parameters_widget.py
@@ -2,6 +2,7 @@
 
 import pytest
 from conftest import STRING_FIELD_MAX_LENGTH
+from pathlib import Path
 
 try:
     from deadline.client.ui.widgets.openjd_parameters_widget import (
@@ -24,6 +25,30 @@ def test_input_in_line_edit_widget_should_be_truncated(qtbot):
     invalid_str = "a" * (STRING_FIELD_MAX_LENGTH + 1)
     widget.set_value(invalid_str)
     assert widget.value() == invalid_str[:STRING_FIELD_MAX_LENGTH]
+
+
+@pytest.mark.parametrize(
+    "object,expected_string",
+    [
+        pytest.param(5, "5", id="Integer to string"),
+        pytest.param(9.4, "9.4", id="Float to string"),
+        pytest.param(Path("/some/path"), "/some/path", id="Path to string"),
+    ],
+)
+def test_input_in_line_edit_widget_should_be_casted(qtbot, object, expected_string):
+    """
+    Tests that inputs to a line edit widget can be converted to strings.
+    """
+
+    # GIVEN
+    widget = _JobTemplateLineEditWidget(None, {"type": "STRING", "name": "value-to-convert"})
+    qtbot.addWidget(widget)
+
+    # WHEN
+    widget.set_value(object)
+
+    # THEN
+    assert widget.value() == expected_string
 
 
 def test_input_in_int_spin_box_widget_should_be_integer_within_range(qtbot):
