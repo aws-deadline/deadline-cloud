7fd738346b952ec53d2740ecea99834325a0b940
Stephen Crowe <6042774+crowecawcaw@users.noreply.github.com>
2025-06-25 14:31:19 -0700

    feat(experimental): Refine the IncrementalDownloadState class
    This commit takes some parts from several in-progress PRs, and
    includes some further docstring refinement and code polishing.
    It also includes some refactoring of the pid file lock functionality.
    
    * Modify the properties of the IncrementalDownloadState, and expand the
      docstring with information about the state and how it evolves.
      * Add downloads_started_timestamp to hold the bootstrap time of the
        incremental download operation.
      * Rename last_lookback_time to downloads_completed_timestamp and have
        it represent the end of the interval over which downloads are
        completed.
      * Add eventual_consistency_max_duration property, so that the
        checkpoint file is more self-documenting.
    * Add an IncrementalDownloadJob class to represent the job within the
      incremental download state.
    * Move _pid_utils.py into the cli module and rename it to
      _pid_file_lock.py. PID file locking is functionality for CLI commands
      to use.
    * Refactor the PID locking tests to use lock files in a temporary
      directory.
    
    Signed-off-by: Mark Wiebe <399551+mwiebe@users.noreply.github.com>
    Signed-off-by: Mark <399551+mwiebe@users.noreply.github.com>

--- src/deadline/job_attachments/incremental_downloads/exceptions.py
+++
@@ -1,9 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-
-"""
-Exceptions for the AWS Deadline Cloud Incremental Download Operations
-"""
-
-
-class PidLockAlreadyHeld(Exception):
-    """Error for when the pid lock is already by a process"""
--- src/deadline/job_attachments/incremental_downloads/incremental_download_state.py
+++ src/deadline/job_attachments/incremental_downloads/incremental_download_state.py
@@ -1,13 +1,82 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
+from __future__ import annotations
+
 import json
 import os
-from datetime import datetime, timezone, timedelta
-from typing import Optional, Callable, List
+from datetime import datetime, timedelta
+from typing import Any, Optional, Callable
+import tempfile
+
+
+class IncrementalDownloadJob:
+    """
+    Model representing a job in the download progress state.
+    """
+
+    _required_dict_fields = ["jobId", "sessions"]
+
+    job_id: str
+    sessions: list
+
+    def __init__(self, job_id: str, sessions: Optional[list] = None):
+        """
+        Initialize a Job instance.
+        Args:
+            job_id (str): The ID of the job
+            sessions (list): List of JobSession objects
+        """
+        self.job_id = job_id
+        self.sessions = sessions or []
+
+    @classmethod
+    def from_dict(cls, data: dict[str, Any]):
+        """
+        Create a Job instance from a dictionary.
+        Args:
+            data (dict): Dictionary containing job data
+        Returns:
+            Job: A new instance populated with the data
+        """
+        if not isinstance(data, dict):
+            raise ValueError("Input must be a dict.")
+        missing_fields = [field for field in cls._required_dict_fields if field not in data]
+        if missing_fields:
+            raise ValueError(f"Input is missing required fields: {missing_fields}")
+
+        sessions = data["sessions"]
+        return cls(job_id=data["jobId"], sessions=sessions)
+
+    def to_dict(self) -> dict[str, Any]:
+        """
+        Convert the Job to a dictionary.
+        Returns:
+            dict: Dictionary representation of the job
+        """
+        return {"jobId": self.job_id, "sessions": self.sessions}
 
 
 class IncrementalDownloadState:
     """
+    Model for tracking all the job attachments downloads to perform for a queue over time.
+    A new download becomes available whenever a TASK_RUN session action completes. The state
+    includes some informational fields that are not strictly necessary, to help make the data
+    on disk easier to understand on inspection.
+
+    * https://docs.aws.amazon.com/deadline-cloud/latest/APIReference/API_GetSessionAction.html#API_GetSessionAction_ResponseSyntax
+    * https://docs.aws.amazon.com/deadline-cloud/latest/APIReference/API_SessionActionDefinition.html
+
+    We track state at three levels, and use the resource state at one level to prune queries at lower levels when we can:
+
+    1. Job - The jobs list contains every job that entered an active status within the time interval [downloads_started_timestamp, downloads_completed_timestamp],
+            where it can generate new task runs, and has not exited as complete or failed.
+    2. Session - Each session of a job represents a single worker running a sequence of tasks from the job. The sessions list in
+            a job contains all the sessions that are active and from which we have downloaded some output.
+    3. SessionAction - Session actions have sequential IDs, so for each session we track the highest index of session action
+            for which we have performed the download.
+
+
+
     Model representing the download progress state file structure.
     Incremental download state file structure:
     {
@@ -43,17 +112,44 @@ class IncrementalDownloadState:
     }
     """
 
-    last_lookback_time: datetime
-    jobs: List[dict]
+    _required_dict_fields = [
+        "downloadsStartedTimestamp",
+        "downloadsCompletedTimestamp",
+        "eventualConsistencyMaxSeconds",
+        "jobs",
+    ]
 
-    def __init__(self, last_lookback_time=None, jobs=None):
+    downloads_started_timestamp: datetime
+    """The timestamp of when the download state was bootstrapped."""
+    downloads_completed_timestamp: Optional[datetime]
+    """The timestamp up to which we are confident downloads are complete."""
+    eventual_consistency_max_duration: timedelta = timedelta(seconds=120)
+    """The duration for deadline:SearchJobs query overlap, to account for eventual consistency."""
+
+    jobs: list[IncrementalDownloadJob]
+    """The list of jobs that entered 'active' status between downloads_started_timestamp and downloads_completed_timestamp, and are not completed."""
+
+    def __init__(
+        self,
+        downloads_started_timestamp: datetime,
+        downloads_completed_timestamp: Optional[datetime] = None,
+        jobs: Optional[list] = None,
+        eventual_consistency_max_duration: Optional[timedelta] = None,
+    ):
         """
-        Initialize a IncrementalDownloadState instance.
+        Initialize a IncrementalDownloadState instance. To bootstrap the state, construct with only the downloads_started_timestamp.
+
         Args:
-            last_lookback_time (datetime): datetime format timestamp of the last lookback time
-            jobs (list): List of job dictionaries containing job_id and sessions information
+            downloads_started_timestamp (datetime): The timestamp of when the download state was bootstrapped.
+            downloads_completed_timestamp (datetime): The timestamp up to which we are confident downloads are complete.
+            jobs (list[IncrementalDownloadJob]): The list of jobs that entered 'active' status between downloads_started_timestamp
+                    and downloads_completed_timestamp, and are not completed.
+            eventual_consistency_max_duration (Optional[timedelta]): The duration for deadline:SearchJobs query overlap, to account for eventual consistency.
         """
-        self.last_lookback_time = last_lookback_time
+        self.downloads_started_timestamp = downloads_started_timestamp
+        self.downloads_completed_timestamp = downloads_completed_timestamp
+        if eventual_consistency_max_duration:
+            self.eventual_consistency_max_duration = eventual_consistency_max_duration
         self.jobs = jobs or []
 
     @classmethod
@@ -65,10 +161,22 @@ class IncrementalDownloadState:
         Returns:
             IncrementalDownloadState: A new instance populated with the data
         """
-        if not data:
-            return cls()
+        if not isinstance(data, dict):
+            raise ValueError("Input must be a dict.")
+        missing_fields = [field for field in cls._required_dict_fields if field not in data]
+        if missing_fields:
+            raise ValueError(f"Input is missing required fields: {missing_fields}")
 
-        return cls(last_lookback_time=data.get("lastLookbackTime"), jobs=data.get("jobs", []))
+        return cls(
+            downloads_started_timestamp=datetime.fromisoformat(data["downloadsStartedTimestamp"]),
+            downloads_completed_timestamp=datetime.fromisoformat(
+                data["downloadsCompletedTimestamp"]
+            ),
+            eventual_consistency_max_duration=timedelta(
+                seconds=int(data["eventualConsistencyMaxSeconds"])
+            ),
+            jobs=data["jobs"],
+        )
 
     def to_dict(self):
         """
@@ -76,36 +184,21 @@ class IncrementalDownloadState:
         Returns:
             dict: Dictionary representation of the state file model
         """
-        return {"lastLookbackTime": self.last_lookback_time.isoformat(), "jobs": self.jobs}
+        result = {
+            "downloadsStartedTimestamp": self.downloads_started_timestamp.isoformat(),
+            "eventualConsistencyMaxSeconds": self.eventual_consistency_max_duration.total_seconds(),
+            "jobs": [job.to_dict() for job in self.jobs],
+        }
+        if self.downloads_completed_timestamp is not None:
+            result["downloadsCompletedTimestamp"] = self.downloads_completed_timestamp.isoformat()
 
-    @classmethod
-    def from_bootstrap(
-        cls,
-        bootstrap_lookback_in_minutes: Optional[int],
-        print_function_callback: Callable[[str], None],
-    ) -> "IncrementalDownloadState":
-        """
-        Bootstraps fresh download progress state using bootstrap lookback if provided or now
-        :param bootstrap_lookback_in_minutes: lookback period in minutes
-        :param print_function_callback: Callback to print messages produced in this function.
-                Used in the CLI to print to stdout using click.echo. By default, ignores messages.
-        :return: returns a fresh download progress state
-        """
-        current_download_progress: IncrementalDownloadState = IncrementalDownloadState()
-
-        print_function_callback(
-            "Bootstrapping command. Ignoring download progress location and creating new"
-        )
-        current_download_progress.last_lookback_time = datetime.now(timezone.utc) - timedelta(
-            minutes=float(bootstrap_lookback_in_minutes or 0)
-        )
-        return current_download_progress
+        return result
 
     @classmethod
     def from_file(
         cls,
-        saved_progress_checkpoint_full_path: str,
-        print_function_callback: Callable[[str], None],
+        file_path: str,
+        print_function_callback: Callable[[str], None] = print,
     ) -> "IncrementalDownloadState":
         """
         Loads progress from state file saved at saved_progress_checkpoint_full_path
@@ -115,70 +208,46 @@ class IncrementalDownloadState:
         :return: Returns the loaded state file,
         or throws an exception if we're unable to read it as we already validated its existence
         """
-        try:
-            state_data: dict = {}
-            with open(saved_progress_checkpoint_full_path, "r") as file:
-                state_data = json.load(file)
+        state_data: dict = {}
+        with open(file_path, "r") as file:
+            state_data = json.load(file)
 
-            current_download_progress: IncrementalDownloadState = (
-                IncrementalDownloadState.from_dict(state_data)
-            )
-            print_function_callback(
-                f"Loaded existing state file from download progress checkpoint location {saved_progress_checkpoint_full_path}"
-            )
-            return current_download_progress
-
-        except Exception as e:
-            print_function_callback(
-                f"Failed to load existing state file from download progress checkpoint location {saved_progress_checkpoint_full_path}: {str(e)}"
-            )
-            # Raise as this is an unexpected exception in reading state file, we already checked its existence earlier
-            raise
+        download_state = IncrementalDownloadState.from_dict(state_data)
+        print_function_callback(
+            f"Loaded existing state file from download progress checkpoint location {file_path}"
+        )
+        return download_state
 
     def save_file(
         self,
-        saved_progress_checkpoint_full_path: str,
-        print_function_callback: Callable[[str], None],
+        file_path: str,
+        print_function_callback: Callable[[str], None] = print,
     ) -> None:
         """
         Save the current download progress to a state file atomically.
 
-        :param saved_progress_checkpoint_full_path: Absolute path of file with saved progress
+        :param file_path: Where to save the file.
         :param print_function_callback: Callback to print messages produced in this function.
                 Used in the CLI to print to stdout using click.echo. By default, ignores messages
-        :return: None if save was successful,
-        or throws an exception if we're unable to save progress file to download location.
-            Hard fail for customer's assets to be downloaded EXACTLY once.
+        :return: None if save was successful, throws an exception if we're unable to save progress file to download location.
         """
+        # Create directory if it doesn't exist
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir, exist_ok=True)
 
-        try:
-            # 1. Create directory if it doesn't exist
-            os.makedirs(os.path.dirname(saved_progress_checkpoint_full_path), exist_ok=True)
+        state_data = self.to_dict()
 
-            # 2. Convert the IncrementalDownloadState to a dictionary
-            state_data = self.to_dict()
+        # Write the data to a unique temporary filename
+        with tempfile.NamedTemporaryFile(
+            mode="w",
+            dir=file_dir,
+            prefix=os.path.basename(file_path),
+            encoding="utf-8",
+            delete=False,
+        ) as tmpfile:
+            json.dump(state_data, tmpfile.file, indent=2)
 
-            # 3. Get a unique id for atomic file update and create a temporary file in the given directory
-            unique_process_identifier = str(os.getpid())
-            temp_file_path = (
-                f"{saved_progress_checkpoint_full_path}_{unique_process_identifier}.tmp"
-            )
+        # Atomically replace the target file with the temporary file
+        os.replace(tmpfile.name, file_path)
 
-            # 4. Write the state data to the temporary file
-            with open(temp_file_path, "w") as file:
-                json.dump(state_data, file, indent=2)
-
-            # 5. Atomically replace the target file with the temporary file
-            os.replace(temp_file_path, saved_progress_checkpoint_full_path)
-
-            print_function_callback(
-                f"Successfully saved state file to {saved_progress_checkpoint_full_path}"
-            )
-        except Exception as e:
-            print_function_callback(
-                f"Failed to save state file to {saved_progress_checkpoint_full_path}: {str(e)}"
-            )
-
-            # Raise as this is an unexpected exception in saving state file,
-            # Hard fail for customer's files to be downloaded EXACTLY once.
-            raise
+        print_function_callback(f"Successfully saved state file to {file_path}")
