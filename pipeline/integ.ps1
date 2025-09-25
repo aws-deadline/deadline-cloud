5461d788d2db8a50d6833c684846e6a82126730f
Eric Osiowy <134728121+erico-aws@users.noreply.github.com>
2025-09-25 15:14:39 -0500

    chore: Revert "chore: pin the version of click that hatch uses to <8.3 due to Sentinel bug (#846)"
    This reverts commit d58e0387fe5a5f0fa50241b05330ad21a5bf0efc.
    
    Signed-off-by: Eric Osiowy <134728121+erico-aws@users.noreply.github.com>

--- pipeline/integ.ps1
+++ pipeline/integ.ps1
@@ -3,6 +3,6 @@
 $ErrorActionPreference = "Stop"
 
 pip install --upgrade pip
-pip install --upgrade hatch "click<8.3"
+pip install --upgrade hatch
 hatch run integ:test
 if ($LASTEXITCODE -ne 0) { throw "Failed to run integration tests" }
