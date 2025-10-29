c52b3f232cac76c3a6bcdf05d3fd7012473b7818
baxeaz <86568017+baxeaz@users.noreply.github.com>
2025-10-29 10:44:01 -0700

    perf: Adding thread local s3 cache db connections to fix performance bottleneck during bundle submission (#896)
    perf: Enabling WAL mode by default for improved concurrency
    
    perf: Adding a get_connection_entry method which accepts a db connection
    parameter to allow for greater read concurrency.  get_connection_entry
    doesn't take the main db_lock which protects the primary db_connection.
    
    perf: Adding a get_local_connection entry which creates (if necessary) a
    thread local db conneciton suitable for reading entries in
    get_connection_entry.
    
    Signed-off-by: Brian Axelson <86568017+baxeaz@users.noreply.github.com>

--- src/deadline/job_attachments/caches/cache_db.py
+++ src/deadline/job_attachments/caches/cache_db.py
@@ -6,6 +6,7 @@ Module for defining a local cache file.
 
 import logging
 import os
+import threading as _threading
 from abc import ABC
 from threading import Lock
 from typing import Optional
@@ -34,6 +35,8 @@ class CacheDB(ABC):
         self.cache_name: str = cache_name
         self.table_name: str = table_name
         self.create_query: str = create_query
+        self._local = _threading.local()
+        self._local_connections: set = set()
 
         try:
             # SQLite is included in Python installers, but might not exist if building python from source.
@@ -64,6 +67,7 @@ class CacheDB(ABC):
                 self.db_connection: sqlite3.Connection = sqlite3.connect(
                     self.cache_dir, check_same_thread=False
                 )
+                self.db_connection.execute("PRAGMA journal_mode=WAL")
             except sqlite3.OperationalError as oe:
                 raise JobAttachmentsError(
                     f"Could not access cache file in {self.cache_dir}"
@@ -81,8 +85,35 @@ class CacheDB(ABC):
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
         """Called when exiting the context manager."""
+
         if self.enabled:
+            import sqlite3
+
             self.db_connection.close()
+            for conn in self._local_connections:
+                try:
+                    conn.close()
+                except sqlite3.Error as e:
+                    logger.warning(f"SQLite connection failed to close with error {e}")
+
+            self._local_connections.clear()
+
+    def get_local_connection(self):
+        """Create and/or returns a thread local connection to the SQLite database."""
+        if not self.enabled:
+            return None
+        import sqlite3
+
+        if not hasattr(self._local, "connection"):
+            try:
+                self._local.connection = sqlite3.connect(self.cache_dir, check_same_thread=False)
+                self._local_connections.add(self._local.connection)
+            except sqlite3.OperationalError as oe:
+                raise JobAttachmentsError(
+                    f"Could not create connection to cache in {self.cache_dir}"
+                ) from oe
+
+        return self._local.connection
 
     @classmethod
     def get_default_cache_db_file_dir(cls) -> Optional[str]:
@@ -99,12 +130,23 @@ class CacheDB(ABC):
         """
         Removes the underlying cache contents from the file system.
         """
+
         if self.enabled:
+            import sqlite3
+
             self.db_connection.close()
+            conn_list = list(self._local_connections)
+            for conn in conn_list:
+                try:
+                    conn.close()
+                    self._local_connections.remove(conn)
+                except sqlite3.Error as e:
+                    logger.warning(f"SQLite connection failed to close with error {e}")
 
         logger.debug(f"The cache {self.cache_dir} will be removed")
         try:
             os.remove(self.cache_dir)
         except Exception as e:
             logger.error(f"Error occurred while removing the cache file {self.cache_dir}: {e}")
+
             raise e
--- src/deadline/job_attachments/caches/hash_cache.py
+++ src/deadline/job_attachments/caches/hash_cache.py
@@ -59,6 +59,35 @@ class HashCache(CacheDB):
             cache_dir=cache_dir,
         )
 
+    def get_connection_entry(
+        self, file_path_key: str, hash_algorithm: HashAlgorithm, connection
+    ) -> Optional[HashCacheEntry]:
+        """
+        Returns an entry from the hash cache, if it exists.  This is the "lockless" (Doesn't take
+        the main db_lock protecting db_connection) version of get_entry which expects a connection
+        parameter for the connection which will be used to read from the DB - this can generally
+        be the thread local connection returned by get_local_connection()
+        """
+        if not self.enabled:
+            return None
+
+        entry_vals = connection.execute(
+            f"SELECT * FROM {self.table_name} WHERE file_path=? AND hash_algorithm=?",
+            [
+                file_path_key.encode(encoding="utf-8", errors="surrogatepass"),
+                hash_algorithm.value,
+            ],
+        ).fetchone()
+        if entry_vals:
+            return HashCacheEntry(
+                file_path=str(entry_vals[0], encoding="utf-8", errors="surrogatepass"),
+                hash_algorithm=HashAlgorithm(entry_vals[1]),
+                file_hash=entry_vals[2],
+                last_modified_time=str(entry_vals[3]),
+            )
+        else:
+            return None
+
     def get_entry(
         self, file_path_key: str, hash_algorithm: HashAlgorithm
     ) -> Optional[HashCacheEntry]:
@@ -69,22 +98,7 @@ class HashCache(CacheDB):
             return None
 
         with self.db_lock, self.db_connection:
-            entry_vals = self.db_connection.execute(
-                f"SELECT * FROM {self.table_name} WHERE file_path=? AND hash_algorithm=?",
-                [
-                    file_path_key.encode(encoding="utf-8", errors="surrogatepass"),
-                    hash_algorithm.value,
-                ],
-            ).fetchone()
-            if entry_vals:
-                return HashCacheEntry(
-                    file_path=str(entry_vals[0], encoding="utf-8", errors="surrogatepass"),
-                    hash_algorithm=HashAlgorithm(entry_vals[1]),
-                    file_hash=entry_vals[2],
-                    last_modified_time=str(entry_vals[3]),
-                )
-            else:
-                return None
+            return self.get_connection_entry(file_path_key, hash_algorithm, self.db_connection)
 
     def put_entry(self, entry: HashCacheEntry) -> None:
         """Inserts or replaces an entry into the hash cache database after acquiring the lock."""
