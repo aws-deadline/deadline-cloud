83c753a4783eac9b1e247643ba6615c7b4d9c8e0
Mark Wiebe <399551+mwiebe@users.noreply.github.com>
2025-12-28 11:44:22 -0800

    feat!: Update hash cache to support byte ranges for file chunking support (#953)
    BREAKING CHANGES:
    - Hash cache database schema updated from V3 to V4.
    
    Signed-off-by: Mark Wiebe <399551+mwiebe@users.noreply.github.com>
    Signed-off-by: Louise Fox <208544511+folouiseAWS@users.noreply.github.com>

--- src/deadline/job_attachments/caches/__init__.py
+++ src/deadline/job_attachments/caches/__init__.py
@@ -1,7 +1,7 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 from .cache_db import CacheDB, CONFIG_ROOT, COMPONENT_NAME
-from .hash_cache import HashCache, HashCacheEntry
+from .hash_cache import HashCache, HashCacheEntry, WHOLE_FILE_RANGE_END
 from .s3_check_cache import S3CheckCache, S3CheckCacheEntry
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     "COMPONENT_NAME",
     "HashCache",
     "HashCacheEntry",
+    "WHOLE_FILE_RANGE_END",
     "S3CheckCache",
     "S3CheckCacheEntry",
 ]
